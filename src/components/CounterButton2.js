d7d30991681af611e17469c460110836c270628c
charandass <charan1631@gmail.com>
2020-01-20 21:26:33 +0530

    helloooo

--- src/components/CounterButton2.js
+++ src/components/CounterButton2.js
@@ -1,29 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class CounterButton2 extends Component {
   constructor(props) {
     super(props);
-    this.state = {count: 0};
+    this.state = { count: 0 };
   }
 
-          shouldComponentUpdate(nextProps,
-           nextState) {
+  shouldComponentUpdate(nextProps, nextState) {
     if (this.state.count !== nextState.count) {
-                            return true;
+      return true;
     }
     return false;
   }
 
   render() {
-      return (
+    return (
       <button
-      id='counter'
+        id="counter"
         color={this.props.color}
-        onClick={() => this.setState(state => ({count: state.count + 1}))}>
+        onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+      >
         Count: {this.state.count}
       </button>
     );
   }
 }
 
-export default CounterButton2
+export default CounterButton2;

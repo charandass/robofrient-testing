bf6fe666701e75480b11571c48f05142dc8f15c3
charandass <charan1631@gmail.com>
2020-01-21 17:05:04 +0530

    doing some more test

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,45 +1,40 @@
-import * as actions from './actions';
+import * as actions from "./actions";
 import {
   CHANGE_SEARCHFIELD,
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
   REQUEST_ROBOTS_FAILED
- } from './constants';
+} from "./constants";
 
-
- import configureMockStore from 'redux-mock-store';
- import thunkMiddleware from 'redux-thunk';
+import configureMockStore from "redux-mock-store";
+import thunkMiddleware from "redux-thunk";
 
 const mockStore = configureMockStore([thunkMiddleware]);
-const nock = require('nock');
-const couchdb = nock("https://jsonplaceholder.typicode.com");
-                
+const nock = require("nock");
 
-it('should create an action to search robots',()=> {
-	const text = 'wooo';
-	const expecctedAction = {
-		type: CHANGE_SEARCHFIELD,
-		payload: text
-	}
-	expect(actions.setSearchField(text)).toEqual(expecctedAction)
-})
+it("should create an action to search robots", () => {
+  const text = "wooo";
+  const expecctedAction = {
+    type: CHANGE_SEARCHFIELD,
+    payload: text
+  };
+  expect(actions.setSearchField(text)).toEqual(expecctedAction);
+});
 
-it('handles requesting robots API', () => {
-	const store = mockStore();
-	store.dispatch(actions.requestRobots())
-	const action = store.getActions();
-	// console.log('action', action)
-	const expecctedAction = {
-		type: REQUEST_ROBOTS_PENDING,
-		}
-	expect(action[0]).toEqual(expecctedAction)
-})
+it("handles requesting robots API", () => {
+  const store = mockStore();
+  store.dispatch(actions.requestRobots());
+  const action = store.getActions();
+  // console.log('action', action)
+  const expecctedAction = {
+    type: REQUEST_ROBOTS_PENDING
+  };
+  expect(action[0]).toEqual(expecctedAction);
+});
 // it('should handle api call',()=> {
-// 	const store = mockStore();
-// 	store.dispatch(actions.requestRobots());
-// 	 couchdb.get("/users/1")
+// 	const couchdb = nock("https://jsonplaceholder.typicode.com")
+// 	.get('/users/1')
 // 	.reply(200,{
-		
 // 	})
 // 	expect(couchdb).toEqual({ type: REQUEST_ROBOTS_SUCCESS, payload: data })
 // })
